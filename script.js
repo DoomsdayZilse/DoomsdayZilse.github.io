1199b5bcd8ed5c43b3d8296cccee53dc5e9206ef
DoomsdayZilse <nicolaizilse09@gmail.com>
2025-09-26 18:38:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let money = 0;
+let item1Cost = 1750;
+let item1Owned = 0;
 
 const moneyDisplay = document.getElementById('moneyDisplay');
 const clickButton = document.getElementById('clickButton');
